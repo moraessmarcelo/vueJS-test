8dbf9db157546829b9f632b125bd7d3f79bc67bc
Marcelo Moraes <marcelo.moraes@Air-de-thb.lan>
2021-01-25 16:10:06 -0300

    data-binding

--- App.vue
+++ App.vue
@@ -1,3 +1,19 @@
 <template> 
-<h1>Hello World</h1> 
-</template> 
+    <div>
+        <h1 :style="{   textDecoration: decoration  }">Hello {{name}}! </h1> 
+        <input type="text" v-model="name"> 
+        <br> 
+
+        <a :href="link"> Link vuejs </a> 
+    </div> 
+</template>
+
+<script> 
+export default {
+    data: () => ({
+        name: 'Marcelo', 
+        decoration: 'underline',
+        link: 'https://cli.vuejs.org/guide/installation.html'
+    })
+}
+</script>
